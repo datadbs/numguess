bbcbebea135708f1278c01afda639f12d93a64ee
datadbs <dj9034kr@gmail.com>
2023-01-09 16:27:35 +0900

    feat: issue2 is done

--- numguess.py
+++ numguess.py
@@ -2,3 +2,9 @@ import random
 
 rand=random.randint(1,200)
 print(rand)
+
+username=input("input user's name >")
+print(f"greeting {username}")
+
+guess=input("assume user's guess")
+print(guess==username)

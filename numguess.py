af109a29d33ecbd0c164912fb6b641a753ca8768
datadbs <dj9034kr@gmail.com>
2023-01-11 15:49:28 +0900

    feat: add some code numguess.py

--- numguess.py
+++ numguess.py
@@ -1,7 +1,7 @@
-import random
-import time
+from random import randint
+from time import sleep
 
-answer=random.randint(1,200)
+answer=randint(1,200)
 print(answer)
 
 username=input("input user's name >")
@@ -13,14 +13,14 @@ guess=int(input("assume user's guess"))
 # Compare answer with user's guess
 if guess==answer:
     print('************')
-    print(1)
+    sleep(1)
     print('************')
-    print(1)
+    sleep(1)
     print('************')
-    print(1)
+    sleep(1)
     print(f'You got it right!! The answer is {answer}!!')
 elif guess>answer:
-    print(f'keep goning, man~! that was too high, {username}..')
+    print(f'keep going, man~! that was too high, {username}..')
 elif guess<answer:
-    print(f'keep goning, man~! that was too low, {username}..')
+    print(f'keep going, man~! that was too low, {username}..')
 

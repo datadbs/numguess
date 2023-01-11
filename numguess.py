e2191113218742483392b8fb85ad1702f45e1346
datadbs <dj9034kr@gmail.com>
2023-01-11 16:17:52 +0900

    feat: add numguess funtion

--- numguess.py
+++ numguess.py
@@ -7,20 +7,29 @@ print(answer)
 username=input("input user's name >")
 print(f"greeting {username}")
 
-guess=int(input("assume user's guess"))
 
+point = 0
 
-# Compare answer with user's guess
-if guess==answer:
-    print('************')
-    sleep(1)
-    print('************')
-    sleep(1)
-    print('************')
-    sleep(1)
-    print(f'You got it right!! The answer is {answer}!!')
-elif guess>answer:
-    print(f'keep going, man~! that was too high, {username}..')
-elif guess<answer:
-    print(f'keep going, man~! that was too low, {username}..')
+def numguess():
+    # Compare answer with user's guess
+    global point
+    guess=int(input("assume user's guess"))
+    if guess==answer:
+        print('************')
+        sleep(1)
+        print('************')
+        sleep(1)
+        print('************')
+        sleep(1)
+        print(f'You got it right!! The answer is {answer}!!')
+        point+=5
+    elif guess>answer:
+        print(f'keep going, man~! that was too high, {username}..')
+    elif guess<answer:
+        print(f'keep going, man~! that was too low, {username}..')
 
+times = int(input("how many times do you want >>"))
+for _ in range(times):
+    numguess()
+
+print(f'{point} points you got')
